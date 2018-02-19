dec3d81e5741976b0d582c659dfcfffc0b01b5eb
bowang <boscodtt@gmail.com>
2018-02-19 21:08:03 +0800

    added documentRoot

--- resolveSsi.js
+++ resolveSsi.js
@@ -1,42 +1,63 @@
-// Learning Functional Programming with Javascript
-// Chapter 04, Video 03, Exercise 01
-var fs = require('fs')
+var fs = require('fs');
+var lodash = require("lodash");
 
-var _ = require("lodash")
+//console.log = function() {};
 
-var resolveSsiInDir = function(dir){
-	var fileList = fs.readdirSync(entryDir);
-	//console.log("fileList", fileList);
+var main = function(){
+	//var filePathList = ["mini-site/hos2018/en/index.html"];
+	var relativeEntryDir = 'mini-site/hos2018/en/';
+	var documentRoot = __dirname + "/document_root/";
 
-	var htmlFileList = _.filter(fileList, function(element) {
+	console.log(arguments.callee.name, relativeEntryDir);
+	console.log(arguments.callee.name, documentRoot);
+	
+	var run = function(){
+		console.log(arguments.callee.name, "start");
+		resolveSsiInDir(documentRoot, relativeEntryDir);
+		console.log(arguments.callee.name, "end");
+	};
+	return {
+		run: run
+	}
+}();
+
+var resolveSsiInDir = function(documentRoot, relativeEntryDir){
+	console.group();
+	var fileList = fs.readdirSync(documentRoot + relativeEntryDir);
+	//console.log(arguments.callee.name, "fileList", fileList);
+
+	var htmlFileList = lodash.filter(fileList, function(element) {
 		return element.endsWith(".html");
-	})
-	//console.log("htmlFileList", htmlFileList);
+	});
+	//console.log(arguments.callee.name, "htmlFileList", htmlFileList);
 
-	var htmlFilePathList = _.map(htmlFileList, function(element) {
-		return entryDir + element;
-	})
-	//console.log("htmlFilePathList", htmlFilePathList);
+	var htmlFilePathList = lodash.map(htmlFileList, function(element) {
+		return documentRoot + relativeEntryDir + element;
+	});
+	//console.log(arguments.callee.name, "htmlFilePathList", htmlFilePathList);
 
+	resolveSsiInFilePathList(documentRoot, htmlFilePathList);
+	console.groupEnd();
+};
 
-	resolveSsiInFilePathList(htmlFilePathList);
-}
-
-var resolveSsiInFilePathList = function(filePathList){
+var resolveSsiInFilePathList = function(documentRoot, filePathList){
+	console.group();
 	// map file path to file content
-	var fileContentList = _.map(filePathList, function(element) {
+	var fileContentList = lodash.map(filePathList, function(element) {
 		return fs.readFileSync(element, 'utf8');
-	})
+	});
 
 	filePathList.forEach(function(element, index, array) {
-		console.log('filePathList[' + index + '] = ' + element);
+		console.log(arguments.callee.name, 'filePathList[' + index + '] = ' + element);
 		
 		fs.readFile(element, 'utf8', function(err, data) {
-		  if (err) throw err
-		  fs.writeFileSync(element+".nossi", resolveSsiInFileContent(fileContentList[index]), 'utf8');
-		})		
-	})
-}
+		  if (err) throw err;
+		  var resolvedContent = resolveSsiInFileContent(documentRoot, fileContentList[index]);
+		  fs.writeFileSync(element+".nossi", resolvedContent, 'utf8');
+		});		
+	});
+	console.groupEnd();
+};
 
 var resolveSsiInFileContent = function(fileContent){
 	var ssiTagList = findSsiTag(fileContent);
@@ -44,33 +65,30 @@ var resolveSsiInFileContent = function(fileContent){
 	if(ssiTagList!=null){
 		ssiTagList.forEach(function(element){
 			var path = ssiTagToSsiPath(element);
-			fileContent = fileContent.replace(element, ssiPathToSsiFileContent(path));
+			fileContent = fileContent.replace(element, ssiPathToSsiFileContent(documentRoot, path));
 		})
 	}
 	
 	return fileContent;
-}
+};
 
 var findSsiTag = function(fileContent){
 	return fileContent.match(/<!--#include[\s\S]*?-->/gm);
-}
+};
 
 var ssiTagToSsiPath = function(tag){
 	var path = tag.match(/(?:(\"|\')(.*?[^\\\\])\1)/g);
 	return path[0].substring(1,path[0].length-1);
-}
+};
 
-var ssiPathToSsiFileContent = function(path){
-	var content = fs.readFileSync(__dirname + path, 'utf8');
+var ssiPathToSsiFileContent = function(documentRoot, path){
+	
+	var content = fs.readFileSync(documentRoot + path, 'utf8');
 	return resolveSsiInFileContent(content);
-}
+};
+
+main.run();
 
 
-
-
-//var filePathList = ["mini-site/hos2018/en/index.html"];
-var entryDir = 'mini-site/hos2018/en/';
-
-resolveSsiInDir(entryDir);
 
 
